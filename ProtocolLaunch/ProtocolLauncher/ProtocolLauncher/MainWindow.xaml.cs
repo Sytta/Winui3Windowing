36c22f87e49610e1f77e7cdf155d47c7f4fa5822
Yawen Hou <yawenhou@microsoft.com>
2023-03-29 16:14:49 -0400

    Add protocol launch test apps

---
+++ ProtocolLaunch/ProtocolLauncher/ProtocolLauncher/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using Microsoft.UI.Xaml;
+using System;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage.Pickers;
+using Windows.System;
+using WinRT.Interop;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace ProtocolLauncher
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        // Please change this to the package family name of the testing app, if testing other apps
+        const string packageName = "17a86331-a6d4-45cd-b3b8-a2aebe4b3f24_07y260wq62y4j";
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void launcAsyncClick(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            var hWnd = WindowNative.GetWindowHandle(this);
+            InitializeWithWindow.Initialize(picker, hWnd);
+            
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".mp4");
+
+            //Select a single file
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+
+            if (file == null)
+                return;
+
+            var inputFile = SharedStorageAccessManager.AddFile(file);
+            LauncherOptions options = new LauncherOptions();
+            options.TargetApplicationPackageFamilyName = packageName;
+
+            var inputData = new ValueSet();
+            inputData.Add("InputToken", inputFile);//input file
+
+            _ = await Launcher.LaunchUriAsync(new Uri("winui3.protocol.launch:", UriKind.Absolute), options, inputData);
+        }
+
+        private async void launchForResultClick(object sender, RoutedEventArgs e) 
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            var hWnd = WindowNative.GetWindowHandle(this);
+            InitializeWithWindow.Initialize(picker, hWnd);
+
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".mp4");
+
+            //Select a single file
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+
+            if (file == null)
+                return;
+
+            var inputFile = SharedStorageAccessManager.AddFile(file);
+            LauncherOptions options = new LauncherOptions();
+
+            options.TargetApplicationPackageFamilyName = packageName;
+
+            var inputData = new ValueSet();
+            inputData.Add("InputToken", inputFile);//input file
+
+            /* Crashes with:
+             Exception thrown: 'System.InvalidOperationException' in WinRT.Runtime.dll
+             An exception of type 'System.InvalidOperationException' occurred in WinRT.Runtime.dll but was not handled in user code
+             */
+            _ = await Launcher.LaunchUriForResultsAsync(new Uri("winui3.protocol.launch:", UriKind.Absolute), options, inputData);
+        }
+    }
+}
---
+++ ProtocolLaunch/ProtocolLauncher/ProtocolLauncherUWP/MainPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ProtocolLauncherUWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        // Please change this to the package family name of the testing app, if testing other apps
+        const string packageName = "17a86331-a6d4-45cd-b3b8-a2aebe4b3f24_07y260wq62y4j";
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void launcAsyncClick(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".mp4");
+
+            //Select a single file
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+
+            if (file == null)
+                return;
+
+            var inputFile = SharedStorageAccessManager.AddFile(file);
+            LauncherOptions options = new LauncherOptions();
+            options.TargetApplicationPackageFamilyName = packageName;
+
+            var inputData = new ValueSet();
+            inputData.Add("InputToken", inputFile);//input file
+
+            _ = await Launcher.LaunchUriAsync(new Uri("winui3.protocol.launch:", UriKind.Absolute), options, inputData);
+        }
+
+        private async void launchForResultClick(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".mp4");
+
+            //Select a single file
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+
+            if (file == null)
+                return;
+
+            var inputFile = SharedStorageAccessManager.AddFile(file);
+            LauncherOptions options = new LauncherOptions();
+
+            options.TargetApplicationPackageFamilyName = packageName;
+
+            var inputData = new ValueSet();
+            inputData.Add("InputToken", inputFile);//input file
+
+            // For example: ms-photos:videoedit?InputToken=123abc&Action=Trim&StartTime=01:02:03
+
+            _ = await Launcher.LaunchUriForResultsAsync(new Uri("winui3.protocol.launch:", UriKind.Absolute), options, inputData);
+        }
+    }
+}
