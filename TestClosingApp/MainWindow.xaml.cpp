a49ae09d6f95e4a1a3d0964aecf92a2e3063f84c
Yawen Hou <yawenhou@microsoft.com>
2024-06-10 16:09:46 -0400

    add protocol launch

--- TestClosingApp/MainWindow.xaml.cpp
+++ TestClosingApp/MainWindow.xaml.cpp
@@ -4,26 +4,23 @@
 #include "MainWindow.g.cpp"
 #endif
 
+#include <winrt/Windows.System.h>
+
 using namespace winrt;
 using namespace Microsoft::UI::Xaml;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
+
 namespace winrt::TestClosingApp::implementation
 {
-    int32_t MainWindow::MyProperty()
-    {
-        throw hresult_not_implemented();
-    }
+    constexpr std::wstring_view c_uri = L"ms-testclosingapp:";
 
-    void MainWindow::MyProperty(int32_t /* value */)
-    {
-        throw hresult_not_implemented();
-    }
-
-    void MainWindow::myButton_Click(IInspectable const&, RoutedEventArgs const&)
+    winrt::Windows::Foundation::IAsyncAction MainWindow::myButton_Click(IInspectable const&, RoutedEventArgs const&)
     {
         myButton().Content(box_value(L"Clicked"));
+        winrt::Windows::Foundation::Uri uri(c_uri);
+        co_await winrt::Windows::System::Launcher::LaunchUriAsync(uri);
     }
 }
--- TestClosingApp/MainWindow.xaml.h
+++ TestClosingApp/MainWindow.xaml.h
@@ -2,20 +2,22 @@
 
 #include "MainWindow.g.h"
 
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Microsoft.UI.Windowing.h>
+
 namespace winrt::TestClosingApp::implementation
 {
     struct MainWindow : MainWindowT<MainWindow>
     {
         MainWindow()
         {
+            AppWindow().TitleBar().ExtendsContentIntoTitleBar(true);
+            AppWindow().TitleBar().PreferredHeightOption(winrt::Microsoft::UI::Windowing::TitleBarHeightOption::Tall);
             // Xaml objects should not call InitializeComponent during construction.
             // See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
         }
 
-        int32_t MyProperty();
-        void MyProperty(int32_t value);
-
-        void myButton_Click(IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        winrt::Windows::Foundation::IAsyncAction myButton_Click(IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
     };
 }
 
